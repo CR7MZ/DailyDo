089fd7317692db138b221b42d14f5f183b79ddb0
李明哲 <963835264@qq.COM>
2023-04-10 10:51:45 +0800

    配置后cocoapods Podfile。跑通PodInstall 支持引入三方库

--- DailyDo/ViewController.swift
+++ DailyDo/ViewController.swift
@@ -6,12 +6,30 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+
+    private lazy var contentView: UIView = {
+        let cv = UIView();
+        cv.backgroundColor = .red;
+
+        return cv;
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupUI()
+    }
+
+    func setupUI() {
+        view.addSubview(contentView)
+        contentView.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(100)
+            make.top.equalToSuperview().offset(100)
+            make.size.equalTo(CGSize(width: 50, height: 50))
+        }
     }
 }
 
